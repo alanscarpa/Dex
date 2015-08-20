1cb98f90f989fbb8f6e487b88c98d420978c6882
Alan Scarpa <alan.scarpa@gmail.com>
2015-08-20 16:38:38 -0400

    QR reader working.  Want to add visual outline

--- SDTestViewController.m
+++ SDTestViewController.m
@@ -1,28 +1,169 @@
-//
-//  SDTestViewController.m
-//  
-//
-//  Created by Alan Scarpa on 8/20/15.
-//
-//
 
+// UYLCaptureViewController.m
+#import <AVFoundation/AVFoundation.h>
 #import "SDTestViewController.h"
 
-@interface SDTestViewController ()
+@interface SDTestViewController () <AVCaptureMetadataOutputObjectsDelegate>
 
+@property (nonatomic, strong) AVCaptureVideoPreviewLayer *previewLayer;
+@property (nonatomic, strong) CALayer *targetLayer;
+@property (nonatomic, strong) AVCaptureSession *captureSession;
+@property (nonatomic, strong) NSMutableArray *codeObjects;
 @end
 
 @implementation SDTestViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self startRunning];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    [self startRunning];
+}
+
+
+- (AVCaptureSession *)captureSession
+{
+    if (!_captureSession)
+    {
+        NSError *error = nil;
+        AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+        if (device.isAutoFocusRangeRestrictionSupported)
+        {
+            if ([device lockForConfiguration:&error])
+            {
+                [device setAutoFocusRangeRestriction:AVCaptureAutoFocusRangeRestrictionNear];
+                [device unlockForConfiguration];
+            }
+        }
+        AVCaptureDeviceInput *deviceInput = [AVCaptureDeviceInput deviceInputWithDevice:device error:&error];
+        
+        if (deviceInput)
+        {
+            _captureSession = [[AVCaptureSession alloc] init];
+            if ([_captureSession canAddInput:deviceInput])
+            {
+                [_captureSession addInput:deviceInput];
+            }
+        }
+        
+        AVCaptureMetadataOutput *metadataOutput = [[AVCaptureMetadataOutput alloc] init];
+        if ([_captureSession canAddOutput:metadataOutput])
+        {
+            [_captureSession addOutput:metadataOutput];
+            [metadataOutput setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
+            [metadataOutput setMetadataObjectTypes:@[AVMetadataObjectTypeQRCode]];
+        }
+        
+        self.previewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:_captureSession];
+        self.previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
+        self.previewLayer.frame = self.view.bounds;
+        [self.view.layer addSublayer:self.previewLayer];
+        
+        self.targetLayer = [CALayer layer];
+        self.targetLayer.frame = self.view.bounds;
+        [self.view.layer addSublayer:self.targetLayer];
+    }
+    
+    return _captureSession;
+
+}
+
+
+- (void)startRunning
+{
+    self.codeObjects = nil;
+    [self.captureSession startRunning];
+}
+
+- (void)stopRunning
+{
+    [self.captureSession stopRunning];
+    self.captureSession = nil;
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [self stopRunning];
+    [super viewWillDisappear:animated];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+
+
 }
+
+- (void)captureOutput:(AVCaptureOutput *)captureOutput
+didOutputMetadataObjects:(NSArray *)metadataObjects
+       fromConnection:(AVCaptureConnection *)connection
+{
+    self.codeObjects = nil;
+    for (AVMetadataObject *metadataObject in metadataObjects)
+    {
+        AVMetadataObject *transformedObject = [self.previewLayer transformedMetadataObjectForMetadataObject:metadataObject];
+        [self.codeObjects addObject:transformedObject];
+        AVMetadataMachineReadableCodeObject *codeObject = (AVMetadataMachineReadableCodeObject *)metadataObject;
+        NSLog(@"%@", codeObject.stringValue);
+    }
+    
+    [self clearTargetLayer];
+    [self showDetectedObjects];
+}
+
+- (void)clearTargetLayer
+{
+    NSArray *sublayers = [[self.targetLayer sublayers] copy];
+    for (CALayer *sublayer in sublayers)
+    {
+        [sublayer removeFromSuperlayer];
+    }
+}
+
+- (void)showDetectedObjects
+{
+    for (AVMetadataObject *object in self.codeObjects)
+    {
+        if ([object isKindOfClass:[AVMetadataMachineReadableCodeObject class]])
+        {
+            CAShapeLayer *shapeLayer = [CAShapeLayer layer];
+            shapeLayer.strokeColor = [UIColor redColor].CGColor;
+            shapeLayer.fillColor = [UIColor clearColor].CGColor;
+            shapeLayer.lineWidth = 2.0;
+            shapeLayer.lineJoin = kCALineJoinRound;
+            CGPathRef path = createPathForPoints([(AVMetadataMachineReadableCodeObject *)object corners]);
+            shapeLayer.path = path;
+            CFRelease(path);
+            [self.targetLayer addSublayer:shapeLayer];
+        }
+    }
+}
+
+CGMutablePathRef createPathForPoints(NSArray* points)
+{
+    CGMutablePathRef path = CGPathCreateMutable();
+    CGPoint point;
+    if ([points count] > 0)
+    {
+        CGPointMakeWithDictionaryRepresentation((CFDictionaryRef)[points objectAtIndex:0], &point);
+        CGPathMoveToPoint(path, nil, point.x, point.y);
+        int i = 1;
+        while (i < [points count])
+        {
+            CGPointMakeWithDictionaryRepresentation((CFDictionaryRef)[points objectAtIndex:i], &point);
+            CGPathAddLineToPoint(path, nil, point.x, point.y);
+            i++;
+        }
+        CGPathCloseSubpath(path);
+    }
+    return path;
+}
+
+// TODO: For brevity I will no tshow the code here but I also stop and start the session when the application moves between the foreground and background. This justt involved listening for the UIApplicationDidEnterBackgroundNotification and UIApplicationWillEnterForegroundNotification notifications
 
 /*
 #pragma mark - Navigation
