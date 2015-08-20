17983aa6e62eedeb2f55ad861df1eda2d6159297
Alan Scarpa <alan.scarpa@gmail.com>
2015-08-20 16:53:29 -0400

    Red outline working

---
+++ Dex/SDCaptureViewController.h
@@ -0,0 +1,13 @@
+//
+//  UYLCaptureViewController.h
+//  Dex
+//
+//  Created by Alan Scarpa on 8/20/15.
+//  Copyright (c) 2015 Skytop Designs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SDCaptureViewController : UIViewController
+
+@end
